8c36b8d9da1985cf7623983c26994a168c55c84b
scott <scott.matthews343@gmail.com>
2024-12-22 12:20:46 +0000

    Update asp.net playground project for OAuth2

--- packages/scalar.aspnetcore/playground/Scalar.AspNetCore.Playground/AuthConstants.cs
+++ packages/scalar.aspnetcore/playground/Scalar.AspNetCore.Playground/AuthConstants.cs
@@ -3,4 +3,5 @@ namespace Scalar.AspNetCore.Playground;
 internal struct AuthConstants
 {
     internal const string ApiKey = "ApiKey";
+    internal const string OAuth2 = "OAuth2";
 }
--- packages/scalar.aspnetcore/playground/Scalar.AspNetCore.Playground/Program.cs
+++ packages/scalar.aspnetcore/playground/Scalar.AspNetCore.Playground/Program.cs
@@ -1,4 +1,6 @@
 using APIWeaver;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Interfaces;
 using Microsoft.OpenApi.Models;
 using Scalar.AspNetCore;
 using Scalar.AspNetCore.Playground;
@@ -27,6 +29,28 @@ builder.Services.AddOpenApi(options =>
         scheme.Type = SecuritySchemeType.ApiKey;
         scheme.In = ParameterLocation.Header;
         scheme.Name = "X-Api-Key";
+    });
+    
+    // Adds OAuth2 security scheme to the api
+    options.AddSecurityScheme(AuthConstants.OAuth2, scheme =>
+    {
+        scheme.Type = SecuritySchemeType.OAuth2;
+        scheme.Flows = new OpenApiOAuthFlows()
+        {
+            AuthorizationCode = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri("https://identityserver/connect/authorize"),
+                TokenUrl = new Uri("https://identityserver/connect/token"),
+                Scopes = new Dictionary<string, string>()
+                {
+                    ["playground-api"] = "Playground API"
+                },
+                Extensions = new Dictionary<string, IOpenApiExtension>()
+                {
+                    ["x-usePkce"] = new OpenApiString("plain")
+                }
+            }
+        };
     });
 
     // Adds 401 and 403 responses to operations
